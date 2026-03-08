917fc6c27cefa39c27b1dd7188fc836ea38019cb
wcriz-cyber <wcriz@proton.me>
2026-03-07 21:30:35 -0500

    fix: scroll MainActivity

--- android/app/src/main/java/com/wcriz/trading5x/MainActivity.java
+++ android/app/src/main/java/com/wcriz/trading5x/MainActivity.java
@@ -1,5 +1,22 @@
 package com.wcriz.trading5x;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        WebView webView = getBridge().getWebView();
+        webView.setOverScrollMode(WebView.OVER_SCROLL_ALWAYS);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.setVerticalScrollBarEnabled(true);
+    }
+}
